a25d0fb72205b6e2c692c390e5a6b3874d882781
Lucas Silva <lucassilvapower@gmail.com>
2022-08-17 16:25:45 -0300

    Firist commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,38 @@
+/*!(function ($) {
+  "use strict";
+
+  // carousel
+  function courseInfinity() {
+    $(".course-home-carousel").owlCarousel({
+      // loop: !0,
+      // nav: true,
+      // dots: !1,
+      loop: true,
+      // margin: 10,
+      nav: false,
+      margin: 15,
+      responsive: {
+        0: { items: 1.25 },
+        460: { items: 2.15 },
+        576: { items: 2.5 },
+        690: { items: 3.15 },
+        768: { items: 3.15 },
+        900: { items: 4.15 },
+      },
+    });
+  }
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }
+
+  $(window).on("load", function () {
+    aos_init();
+    courseInfinity();
+  });
+})(jQuery);
+*/
